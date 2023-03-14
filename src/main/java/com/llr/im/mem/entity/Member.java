2f0b11002fd2345c0336784495409c628d9ecf71
2dosilver <rayyo5720@gmail.com>
2023-03-14 10:21:33 +0900

    Member Entity modify add File properties

--- src/main/java/com/llr/im/mem/entity/Member.java
+++ src/main/java/com/llr/im/mem/entity/Member.java
@@ -1,14 +1,12 @@
 package com.llr.im.mem.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.CreatedDate;
 
 import java.util.Date;
@@ -26,41 +24,49 @@ public class Member {
     private String userId;
 
     @Column(length = 100, nullable = false)
-    private String password;
+    private String userPassword;
 
     @Column(length = 40, nullable = false)
-    private String name;
+    private String userName;
 
     @Column(length = 20, nullable = false)
-    private String phone;
+    private String userPhone;
 
     @Column(nullable = false)
-    private Date birthdate;
+    private String userBirthdate;
 
     @Column(length = 2, nullable = false)
-    private String sex;
+    private String userSex;
 
     @Email
     @Column(length = 100, nullable = false)
-    private String email;
+    private String userEmail;
+
+    @Column(nullable = false)
+    private String fileName;
+
+    @Column(nullable = false)
+    private String fileOriName;
+
+    @Column(nullable = false)
+    private Long fileSize;
 
     @CreatedDate
     @Column(nullable = false, updatable = false)
     private Date regDate;
 
-    @Column(nullable = true, updatable = true)
-    private Date delDate;
-
     @Builder
-    public Member(String userId, String password, String name, String phone, Date birthdate, String sex, String email, Date regDate, Date delDate) {
+    public Member(String userId, String userPassword, String userName, String userPhone,
+                  String userBirthdate, String userSex, String userEmail, String fileName, String fileOriName, Long fileSize) {
         this.userId = userId;
-        this.password = password;
-        this.name = name;
-        this.phone = phone;
-        this.birthdate = birthdate;
-        this.sex = sex;
-        this.email = email;
-        this.regDate = regDate;
-        this.delDate = delDate;
+        this.userPassword = userPassword;
+        this.userName = userName;
+        this.userPhone = userPhone;
+        this.userBirthdate = userBirthdate;
+        this.userSex = userSex;
+        this.userEmail = userEmail;
+        this.fileName = fileName;
+        this.fileOriName = fileOriName;
+        this.fileSize = fileSize;
     }
 }

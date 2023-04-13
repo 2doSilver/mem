c6fa9b5f5b4daf2eac69b8bd29f70b3082dee530
juuuuu <juhyun9086@naver.com>
2023-04-13 10:12:32 +0900

    roomcode비교

--- src/main/java/com/llr/im/mem/controller/room/RoomJoinController.java
+++ src/main/java/com/llr/im/mem/controller/room/RoomJoinController.java
@@ -1,6 +1,9 @@
 package com.llr.im.mem.controller.room;
 
+import com.llr.im.mem.entity.room.Room;
+import com.llr.im.mem.entity.room.RoomRepository;
 import com.llr.im.mem.service.member.room.RoomJoinService;
+import com.llr.im.mem.service.member.room.RoomService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,6 +21,7 @@ import org.springframework.web.bind.annotation.*;
 public class RoomJoinController {
 
     private final RoomJoinService roomJoinService;
+    private final RoomService roomService;
 
     @GetMapping(value = "/{roomId}/join")
     public String joinRoom(Model model, @PathVariable("roomId") Long roomId) {
@@ -31,6 +35,15 @@ public class RoomJoinController {
 
     @PostMapping(value = "/{roomId}/join")
     public String joinRoom(@PathVariable("roomId") Long roomId, @Valid RoomJoinForm roomJoinForm, BindingResult bindingResult) {
+
+        String roomCodeinJoin = roomJoinForm.getRoomCode();
+        Room room = roomService.getRoom(roomId);
+        String roomCodeinCreate = room.getRoomCode();
+
+        if (!roomCodeinJoin.equals(roomCodeinCreate)) {
+            bindingResult.rejectValue("roomCodeinJoin","roomCodeInCorrect", "방 입장코드가 동일하지 않습니다.");
+            return "room_join";
+        }
         try {
             roomJoinService.join(roomJoinForm.getRoomCode(),roomJoinForm.getActiveName(), roomId);
         }catch (DataIntegrityViolationException e) {
